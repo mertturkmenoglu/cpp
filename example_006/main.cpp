6470592b5af650bed5fa3098b01b37a81a86cd43
Mert <mertturkmenoglu99@gmail.com>
2019-06-10 01:29:30 +0300

    Add friend function example

---
+++ example_006/main.cpp
@@ -0,0 +1,34 @@
+/**
+ * friend function example
+ * 
+ * g++ main.cpp -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
+ * valgrind --leak-check=yes -v ./main
+ */
+#include <iostream>
+#include <string>
+
+// Class definition
+class Person {
+private:
+    std::string name;
+    int age;
+public:
+    // Constructor
+    Person(std::string name, int age): name(name), age(age) { }
+    
+    //friend function declaration
+    friend void printInfo(Person);
+};
+
+// Friend function definition
+void printInfo(Person p) {
+    std::cout << "Name: " << p.name << " Age: " << p.age << std::endl;
+}
+
+int main() {
+    Person p("John", 40);
+    std::cout<<"Personal info: "<< std::endl;
+    printInfo(p);
+
+    return 0;
+}
