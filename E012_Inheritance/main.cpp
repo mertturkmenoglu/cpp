055da5c0281f6c5af2ea4696c07dd22291162dbd
Mert <mertturkmenoglu99@gmail.com>
2019-06-11 20:28:51 +0300

    Add multiple inheritance ambiquity resolution example

---
+++ E012_Inheritance/main.cpp
@@ -0,0 +1,37 @@
+/**
+ * Inheritance ambiquity resolution example
+ * 
+ * g++ main.cpp -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <iostream>
+
+class A {
+public:
+    void classInfo() {
+        std::cout << "I'm at class A" << std::endl;
+    }
+};
+
+class B {
+public:
+    void classInfo() {
+        std::cout << "I'm at class B" << std::endl;
+    }
+};
+
+class C : public A, public B {
+public:
+    void printInfo() {
+        A::classInfo();
+        B::classInfo();
+    }
+};
+
+int main() {
+    C c;
+    c.printInfo();
+
+    return 0;
+}
