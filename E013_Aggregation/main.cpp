c53850371d55695cbb02eb11d6a9ed649c7cec6d
Mert <mertturkmenoglu99@gmail.com>
2019-06-12 00:02:19 +0300

    Add aggregation example

---
+++ E013_Aggregation/main.cpp
@@ -0,0 +1,43 @@
+/**
+ * Aggregation(has-a) example
+ * 
+ * g++ main.cpp -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <iostream>
+
+class Address {
+public:
+    std::string street;
+    int no;
+    std::string city;
+
+    Address(std::string street, int no, std::string city) : 
+        street(std::move(street)), no(no), city(std::move(city)) { }
+};
+
+class Student {
+private:
+    Address* address;
+public:
+    int id;
+    std::string name;
+
+    Student(Address *address, int id, std::string name) : 
+        address(address), id(id), name(std::move(name)) { }
+
+    void printInfo() {
+        std::cout << id << " " << name << " " << std::endl;
+        std::cout << address->street << " " << address->no << " " << address->city << std::endl;
+    }
+};
+
+// Driver code
+int main(void) {
+    Address address = Address("Ataturk Street", 19, "Istanbul");
+    Student student = Student(&address, 1923, "Kemal");
+    student.printInfo();
+
+    return 0;
+}
