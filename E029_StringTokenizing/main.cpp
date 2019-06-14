2b661723e248b54862a0ca3acc58185700cca68b
Mert <mertturkmenoglu99@gmail.com>
2019-06-14 05:23:08 +0300

    Add string tokenizing example

---
+++ E029_StringTokenizing/main.cpp
@@ -0,0 +1,28 @@
+/**
+ * String tokenizing example
+ * g++ main.cpp -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+int main() {
+    std::string line = "String contains multiple words";
+    std::vector <std::string> tokens;
+
+    std::stringstream str_stream(line);
+
+    std::string temp;
+
+    while(getline(str_stream, temp, ' ')) {
+        tokens.push_back(temp);
+    }
+
+    for(std::string str : tokens) {
+        std::cout << str << std::endl;
+    }
+
+    return 0;
+}
