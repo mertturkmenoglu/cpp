b0a56fe515bef41b50952299fbb3495e5f33fe5f
Mert <mertturkmenoglu99@gmail.com>
2019-06-14 05:09:38 +0300

    Add array size example

---
+++ E028_ArraySize/main.cpp
@@ -0,0 +1,29 @@
+/**
+ * Finding array size in a different way
+ * g++ main.cpp -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+int main() {
+    srand(time(NULL));
+
+    int first[10];
+    int second[20];
+
+    int *third = new int[42];
+
+    std::cout << "First array: " << (*(&first + 1) - first) << std::endl;
+    std::cout << "Second array: " << (*(&second  + 1) - second) << std::endl;
+
+    // It won't work
+    std::cout << "Third array: " << (*(&third + 1) - third) << std::endl;
+    
+    delete[] third;
+
+    return 0;
+}
+
