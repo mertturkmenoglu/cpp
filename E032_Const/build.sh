8a99c2b8f2f6d98ed36261b7b4e476e9bdf4d0f0
Mert <mertturkmenoglu99@gmail.com>
2019-06-15 19:07:29 +0300

    Add building scripts

---
+++ E032_Const/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+build_command="g++ main.cpp -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main"
+
+clean_command="rm -f main"
+
+if [ $# -gt 0 ] && [ $1 = 'clean' ]
+then
+    eval $clean_command
+else
+    eval $build_command
+fi
