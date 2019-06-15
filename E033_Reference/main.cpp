eeb2636281f9009843b225019c1b383e839e6921
Mert <mertturkmenoglu99@gmail.com>
2019-06-15 20:34:30 +0300

    Add reference example

---
+++ E033_Reference/main.cpp
@@ -0,0 +1,55 @@
+/**
+ * Reference example
+ * g++ main.cpp -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <iostream>
+
+
+void incrementViaPointer(int* value)
+{
+    (*value)++;
+}
+
+void incrementViaReference(int& value)
+{
+    value++;
+}
+
+class JustClass {
+private:
+    std::string m_string;
+public:
+    JustClass(std::string& string) : m_string(string) { }
+    
+    std::string GetString() const {
+        return m_string;
+    }
+};
+
+int main() {
+    int var = 5;
+    int* ptr = &var;
+    int& ref = var;
+
+    std::cout << "Variable: " << var << std::endl;
+    std::cout << "Via pointer: " << *ptr << std::endl;
+    std::cout << "Via reference: " << ref << std::endl;
+
+    incrementViaPointer(ptr);
+    std::cout << "After incrementing via pointer: " << *ptr << std::endl;
+
+    incrementViaReference(ref);
+    std::cout << "After incrementing via reference: " << ref << std::endl;
+
+    std::string my_string = "My string in main function";
+    std::cout << "Before object creation: " << my_string << std::endl;
+    
+    JustClass jc(my_string);
+    std::cout << "After object creation: " << my_string << std::endl;
+    std::cout << "Getter metod call: " << jc.GetString() << std::endl;
+
+    return 0;
+}
+
