0038e890f0fc5a8b58dcd530ce231e07ee1f8c18
Mert <mertturkmenoglu99@gmail.com>
2019-06-19 03:33:51 +0300

    Add unary operator overloading example

---
+++ E039_OperatorOverloading/main.cpp
@@ -0,0 +1,64 @@
+/**
+ * Operator overloading example
+ * g++ main.cpp -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <iostream>
+
+// Overloaded for minus(-), increment(++) and
+// decrement(--) operators
+class Temperature
+{
+private:
+    double m_Value;
+public:
+    Temperature(double value) : m_Value(value) { }
+
+    double GetValue() const {
+        return m_Value;
+    }
+
+    void operator- () {
+        m_Value = -m_Value;
+    }
+
+    // Pre-increment, return object
+    Temperature operator++() {
+        return Temperature(++m_Value);
+    }
+
+    // Post-increment, return object
+    Temperature operator++(int) {
+        return Temperature(m_Value++);
+    }
+
+    // Pre-decrement, return object
+    Temperature operator--() {
+        return Temperature(--m_Value);
+    }
+
+    // Post-decrement, return object
+    Temperature operator--(int) {
+        return Temperature(m_Value--);
+    }
+};
+
+int main() {
+    Temperature t(32);
+    std::cout << t.GetValue() << std::endl;
+
+    -t;
+    std::cout << t.GetValue() << std::endl;
+
+    t++;
+    std::cout << t.GetValue() << std::endl;
+    
+    Temperature t2 = t++;
+    std::cout << t2.GetValue() << std::endl;
+
+    std::cout << (--t2).GetValue() << std::endl;
+
+    return 0;
+}
+
