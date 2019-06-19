66c98fc3ff139c402591495344d8d87a0a54fb01
Mert <mertturkmenoglu99@gmail.com>
2019-06-20 00:07:17 +0300

    Add prime number example

---
+++ E031_Practice/main.cpp
@@ -0,0 +1,36 @@
+/**
+ * Prime number example
+ * "g++ main.cpp -std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main"
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <algorithm>
+#include <numeric>
+#include <list>
+
+int main() {
+    int max_prime = -1;
+    std::vector<int> vec = { 2, 3, 1, 0, 2, 2, 13, 7, 9, 11, 4, -2, 3};
+
+    std::for_each(vec.begin(), vec.end(), [&max_prime](const int& num){
+        if (num > max_prime) {
+            std::list<int> l(num - 1);
+            std::iota(l.begin(), l.end(), 2);
+
+            bool result = std::any_of(l.begin(), l.end(), [num](int i) {
+                return num % i == 0;
+            });
+
+            if (result) {
+                max_prime = num;
+            }
+        }
+    });
+
+    std::cout << max_prime << std::endl;
+
+    return 0;
+}
