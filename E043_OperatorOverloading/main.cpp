4bb4d144c31531f0adaeb7d576110f670bef1477
Mert <mertturkmenoglu99@gmail.com>
2019-06-19 13:05:06 +0300

    Add stream operator overloading example

---
+++ E043_OperatorOverloading/main.cpp
@@ -0,0 +1,38 @@
+/**
+ * Operator Overloading example
+ * g++ main.cpp std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <iostream>
+ 
+class Temperature
+{
+private:
+    double m_Value;
+public:
+    Temperature() : m_Value(-273.15) { }
+    
+    Temperature(double value) : m_Value(value) { }
+    
+    friend std::ostream& operator<<(std::ostream& output, const Temperature& T) {
+        output << T.m_Value;
+        return output;
+    }
+
+    friend std::istream& operator>>(std::istream& input, Temperature& T) {
+        input >> T.m_Value;
+        return input;
+    }
+};
+
+
+int main() {
+    Temperature t;
+    std::cout << "Enter temperature: " << std::endl;
+    std::cin >> t;
+
+    std::cout << "Temperature: " << t << std::endl;
+    return 0;
+}
+
