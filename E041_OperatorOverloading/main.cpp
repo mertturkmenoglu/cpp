e6bc69e1e63e1bd1a3791be3e7691e677e6c9625
Mert <mertturkmenoglu99@gmail.com>
2019-06-19 04:00:13 +0300

    Add relational operator overloading example

---
+++ E041_OperatorOverloading/main.cpp
@@ -0,0 +1,58 @@
+/**
+ * Operator overloading example
+ * g++ main.cpp -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <iostream>
+
+class Temperature
+{
+private:
+    int m_Value;
+public:
+    Temperature(int value) : m_Value(value) { }
+
+    int GetValue() const {
+        return m_Value;
+    }
+
+    bool operator<(const Temperature& other) const {
+        return (this->m_Value < other.m_Value);
+    }
+
+    bool operator>(const Temperature& other) const {
+        return (this->m_Value + other.m_Value);
+    }
+
+    bool operator<=(const Temperature& other) const {
+        return (this->m_Value <= other.m_Value);
+    }
+
+    bool operator>=(const Temperature& other) const {
+        return (this->m_Value >= other.m_Value);
+    }
+
+    bool operator==(const Temperature& other) const {
+        return (this->m_Value == other.m_Value);
+    }
+
+    bool operator!=(const Temperature& other) const {
+        return (this->m_Value != other.m_Value);
+    }
+};
+
+int main() {
+    Temperature t1(8);
+    Temperature t2(5);
+
+    std::cout << ((t1 < t2)? "True" : "False") << std::endl;
+    std::cout << ((t1 > t2)? "True" : "False") << std::endl;
+    std::cout << ((t1 <= t2)? "True" : "False") << std::endl;
+    std::cout << ((t1 >= t2)? "True" : "False") << std::endl;
+    std::cout << ((t1 == t2)? "True" : "False") << std::endl;
+    std::cout << ((t1 != t2)? "True" : "False") << std::endl;
+
+    return 0;
+}
+
