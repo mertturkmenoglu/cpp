5c9a300148257c0d251d4ce44b08f9155e350d33
Mert <mertturkmenoglu99@gmail.com>
2019-06-20 14:50:09 +0300

    Add generic function practice

---
+++ practice/P001_MinMax/main.cpp
@@ -0,0 +1,34 @@
+/**
+ * Practice 001: Minimum Maximum Functions
+ * "g++ main.cpp -std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main"
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <iostream>
+
+template<class T>
+T Min(T a, T b) {
+    return (a < b)? a : b;
+}
+
+template<class T>
+T Max(T a, T b) {
+    return (a > b)? a : b;
+}
+
+int main() {
+
+    int max1 = Max(3, 1);
+    double max2 = Max(2.3, 5.1);
+
+    int min1 = Min(-1, 4);
+    float min2 = Min(3.2f, 6.99f);
+
+    std::cout << max1 << std::endl;
+    std::cout << max2 << std::endl;
+    std::cout << min1 << std::endl;
+    std::cout << min2 << std::endl;
+
+    return 0;
+}
+
