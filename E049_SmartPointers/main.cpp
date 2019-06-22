c1605a76991ab9965eff95fa0244bf2440d92f57
Mert <mertturkmenoglu99@gmail.com>
2019-06-22 18:22:50 +0300

    Add smart pointer implementation

---
+++ E049_SmartPointers/main.cpp
@@ -0,0 +1,49 @@
+/**
+ * Smart Pointer implementation
+ * "g++ main.cpp -std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main"
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <iostream>
+
+template <class T>
+class SpecialPointer {
+private:
+    T* ptr;
+public:
+    explicit SpecialPointer(T* p = nullptr) : ptr(p) {
+        std::cout << "Construct" << std::endl;
+    }
+
+    ~SpecialPointer() {
+        std::cout << "Delete" << std::endl;
+        delete ptr;
+    }
+
+    inline T& operator*() { return *ptr; }
+
+    inline T* operator->() { return ptr; }
+};
+
+class Person {
+private:
+    std::string name;
+public:
+
+    Person(const std::string& name) : name(name) { }
+
+    inline const std::string& GetName() const { return name; }
+    inline void SetName(const std::string& name) { this->name = name; }
+};
+
+int main() {
+    // Block
+    {
+        SpecialPointer<Person> p(new Person(std::string("Ali")));
+        std::cout << p->GetName() << std::endl;
+    }
+    // Destructor should be called
+    
+    return 0;
+}
+
