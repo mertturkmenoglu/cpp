181fa6f22f45a3ff606eeb2791b72fb2871c5d53
Mert <mertturkmenoglu99@gmail.com>
2019-07-22 21:01:26 +0300

    Add STL Algorithm example
    Add find_if() example

---
+++ stl/E007_FindIf/main.cpp
@@ -0,0 +1,38 @@
+/**
+ * find() example
+ * "g++ main.cpp -std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main"
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    std::srand(std::time(nullptr));
+    int n = std::rand() % 10;
+    std::vector<int> numbers(n);
+
+    for (int i = 0; i < n; i++) {
+        numbers.push_back(std::rand() % n);
+    }
+
+    int index = 0;
+    for (int num : numbers) {
+        std::cout << index++ << ": " << num << std::endl;
+    }
+
+    auto item = std::find_if(numbers.begin(), numbers.end(), [](int i) {
+        return i % 2 != 0;
+    });
+
+    if (item != numbers.end()) {
+        std::cout << "Number: " << *item << std::endl;
+    } else {
+        std::cout << "No occurrence found" << std::endl;
+    }
+
+    return 0;
+}
