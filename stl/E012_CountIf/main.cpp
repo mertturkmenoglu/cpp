636f34de9777adb70875cc71bc005bfe6c20c4a3
Mert <mertturkmenoglu99@gmail.com>
2019-07-23 04:39:37 +0300

    Add STL Algorithm example
    Add count_if() example

---
+++ stl/E012_CountIf/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * count() example
+ * "g++ main.cpp -std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main"
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+
+int main() {
+    std::vector<int> numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8 , 9 };
+
+    int counter = std::count_if(numbers.begin(), numbers.end(), [](int n) {
+        return n % 2 == 1;
+    });
+
+    std::cout << "Odd count: " << counter << std::endl;
+
+    return 0;
+}
