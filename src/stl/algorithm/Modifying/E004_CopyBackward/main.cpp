b8d0e1adbff47c33ebd6717f4050bb9740b0fd52
Mert <mertturkmenoglu99@gmail.com>
2019-07-28 23:02:23 +0300

    Add STL Algorithm example
    Add copy_backward() example

---
+++ src/stl/algorithm/Modifying/E004_CopyBackward/main.cpp
@@ -0,0 +1,25 @@
+/**
+ * copy_if() example
+ * g++ main.cpp -std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main() {
+    std::vector<int> numbers = { 1, 2, 3, 4, 5 };
+    std::vector<int> newNumbers;
+    newNumbers.resize(numbers.size() + 10);
+
+    std::copy_backward(numbers.begin(), numbers.end(), newNumbers.end());
+
+    for (const int& number : newNumbers) {
+        std::cout << ' ' << number;
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
