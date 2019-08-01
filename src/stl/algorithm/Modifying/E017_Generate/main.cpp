1c181f64e7dcf69ea79b461aa17890d5efa9df2e
Mert <mertturkmenoglu99@gmail.com>
2019-08-01 15:41:14 +0300

    Add STL Algorithm example
    Add generate() example

---
+++ src/stl/algorithm/Modifying/E017_Generate/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * generate() example
+ * g++ main.cpp -std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+int main() {
+    std::vector<int> vector(10);
+
+    int i = 0;
+    std::generate(vector.begin(), vector.end(), [&i]() {
+        return i++;
+    });
+
+    std::copy(vector.begin(), vector.end(), std::ostream_iterator<int>(std::cout, " "));
+
+    return 0;
+}
