2d9084671d3a3a4deb62b047186e3309c87bd180
Mert <mertturkmenoglu99@gmail.com>
2019-08-01 14:20:37 +0300

    Add STL Algorithm example
    Add replace_if() example

---
+++ src/stl/algorithm/Modifying/E012_ReplaceIf/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * replace_if() example
+ * g++ main.cpp -std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+int main() {
+    std::vector<int> vector = { 1, 1, 2, 3, 5, 8 };
+    std::copy(vector.begin(), vector.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    std::replace_if(vector.begin(), vector.end(), [](int n) { return n % 2 == 1; }, 0);
+
+    std::copy(vector.begin(), vector.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+}
