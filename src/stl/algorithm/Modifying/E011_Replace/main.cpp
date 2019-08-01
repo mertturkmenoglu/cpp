89aecf04b17fa3ea5a874ff313a1f23eb867e489
Mert <mertturkmenoglu99@gmail.com>
2019-08-01 14:17:53 +0300

    Add STL Algorithm example
    Add replace() example

---
+++ src/stl/algorithm/Modifying/E011_Replace/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * replace() example
+ * g++ main.cpp -std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+int main() {
+    std::vector<int> vector = { 1, 1, 2, 3, 5, 8 };
+    std::copy(vector.begin(), vector.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    std::replace(vector.begin(), vector.end(), 1, 0);
+
+    std::copy(vector.begin(), vector.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    return 0;
+}
