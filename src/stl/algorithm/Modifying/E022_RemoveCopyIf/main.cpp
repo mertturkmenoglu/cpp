9cfa0bdebc6f006fade99c67164c3266ee85bb54
Mert <mertturkmenoglu99@gmail.com>
2019-08-01 16:05:53 +0300

    Add STL Algorithm example
    Add remove_copy_if() example

---
+++ src/stl/algorithm/Modifying/E022_RemoveCopyIf/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * remove_copy_if() example
+ * g++ main.cpp -std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+int main() {
+    std::vector<int> vector = { 1, 1, 2, 3, 5, 8 };
+    std::vector<int> result(vector.size());
+
+    std::remove_copy_if(vector.begin(), vector.end(), result.begin(), [](int n) { return n % 2 == 0; });
+
+    std::copy(vector.begin(), vector.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    std::copy(result.begin(), result.end(), std::ostream_iterator<int>(std::cout, " "));
+
+    return 0;
+}
