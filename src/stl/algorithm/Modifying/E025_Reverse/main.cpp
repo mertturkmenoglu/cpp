51c0487415d6aeea075295a98b25bb4c5ba4ed8f
Mert <mertturkmenoglu99@gmail.com>
2019-08-01 16:27:03 +0300

    Add STL Algorithm example
    Add reverse() example

---
+++ src/stl/algorithm/Modifying/E025_Reverse/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * reverse() example
+ * g++ main.cpp -std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+int main() {
+    std::string name = "emily";
+
+    std::cout << name << std::endl;
+
+    std::reverse(name.begin(), name.end());
+
+    std::cout << name << std::endl;
+
+    return 0;
+}
