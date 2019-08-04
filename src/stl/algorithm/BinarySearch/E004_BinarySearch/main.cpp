562c99d3258a824dd2be8cebbb110746833ab656
Mert <mertturkmenoglu99@gmail.com>
2019-08-04 19:14:36 +0300

    Add STL Algorithm example
    Add binary_search() example

---
+++ src/stl/algorithm/BinarySearch/E004_BinarySearch/main.cpp
@@ -0,0 +1,30 @@
+/**
+ * binary_search() example
+ * "g++ main.cpp -std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main"
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+int main() {
+    std::vector<int> vector = { 5, 1, 6, 2, 7, 3, 8, 4 };
+    std::sort(vector.begin(), vector.end());
+
+    std::copy(vector.begin(), vector.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    const int n = 6;
+
+    auto result = std::binary_search(vector.begin(), vector.end(), n);
+
+    if (result) {
+        std::cout << n << " found" << std::endl;
+    } else {
+        std::cout << "Not Found" << std::endl;
+    }
+
+    return 0;
+}
