8049460aa0d6d380c08050e46ad88bcd08b571fa
Mert <mertturkmenoglu99@gmail.com>
2019-08-05 11:45:45 +0300

    Add STL Algorithm example
    Add min() example

---
+++ src/stl/algorithm/MinMax/E003_Min/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * min() example
+ * "g++ main.cpp -std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main"
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <algorithm>
+#include <iostream>
+
+int main() {
+    int min = std::min(5, 4);
+    std::cout << min << std::endl;
+
+    min = std::min({ 5, 1, 6, 2, 7, 3, 8, 4 });
+    std::cout << min << std::endl;
+
+    return 0;
+}
