cfb396d5e844c31ee7e1146b94af47857c0f954a
Mert <mertturkmenoglu99@gmail.com>
2019-08-05 10:55:46 +0300

    Add STL Algorithm example
    Add pop_heap() example

---
+++ src/stl/algorithm/Heap/E003_PopHeap/main.cpp
@@ -0,0 +1,38 @@
+/**
+ * pop_heap() example
+ * "g++ main.cpp -std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main"
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+int main() {
+    std::vector<int> vector = { 5, 1, 2, 6, 3, 8, 4 };
+
+    std::cout << "Initial vector: ";
+    std::copy(vector.begin(), vector.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    std::make_heap(vector.begin(), vector.end());
+
+    std::cout << "After heapify:  ";
+    std::copy(vector.begin(), vector.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    std::pop_heap(vector.begin(), vector.end());
+
+    std::cout << "After std::pop_heap():  ";
+    std::copy(vector.begin(), vector.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    vector.pop_back();
+
+    std::cout << "After pop_back(): ";
+    std::copy(vector.begin(), vector.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    return 0;
+}
