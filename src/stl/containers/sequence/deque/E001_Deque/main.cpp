695b85578c0748e5a3bd8402fd249b73b65ebca9
Mert <mertturkmenoglu99@gmail.com>
2019-08-23 10:19:38 +0300

    Add STL Container example
    Add Deque example

---
+++ src/stl/containers/sequence/deque/E001_Deque/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * STL Container: Deque example
+ * "g++ main.cpp -std=c++17 -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main"
+ * valgrind --leak-check=yes -v ./main
+ */
+
+#include <algorithm>
+#include <deque>
+#include <iostream>
+#include <iterator>
+
+int main() {
+    std::deque<int> deq = { 1, 2, 3 };
+    std::copy(deq.begin(), deq.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    deq.push_back(4);
+    std::copy(deq.begin(), deq.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    deq.push_front(0);
+    std::copy(deq.begin(), deq.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    return 0;
+}
