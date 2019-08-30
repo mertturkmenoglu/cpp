6a56ce3258b9015b4c1549921eb67676e9515bb9
Mert <mertturkmenoglu99@gmail.com>
2019-08-30 18:53:25 +0300

    Add information on implicit conversion example

--- src/examples/advanced/E002_ImplicitConversion/main.cpp
+++ src/examples/advanced/E002_ImplicitConversion/main.cpp
@@ -1,40 +1,49 @@
-#include <utility>
-
 /**
  * Implicit type conversion example
- * g++ main.cpp -Wall -Wextra -Wnon-virtual-dtor -pedantic -g -o main
- * valgrind --leak-check=yes -v ./main
+ *
+ * When we create objects of a class, sometimes we want to create them in a more conscious way.
+ * In this example, we have a Person class and a Person has a name and a age.
+ * Sometimes we want to create a Person object but we do not want to give all information.
+ * Just name or just age.
+ * Here we have two constructors, they initialize the object with appropriate values but we give only
+ * one information. An int value or an std::string value. Compiler will analyze the context and try to find
+ * a constructor which is not marked as explicit. Then it will implicitly converse the type to Person.
  */
 
 #include <iostream>
 
 class Person
 {
-private:
-    std::string m_Name;
-    int m_Age;
 public:
-    Person(std::string  name)
-        : m_Name(std::move(name)), m_Age(-1) { }
+    std::string mName;
+    int mAge;
+public:
+    Person(std::string name)
+        : mName(std::move(name)), mAge(-1) { }
 
     Person(int age)
-        : m_Name(""), m_Age(age) { }
+        : mName(""), mAge(age) { }
 };
 
 void printPerson(const Person& p) {
-    std::cout << "printPerson()" << std::endl;
+    std::cout << "Name: " << p.mName << " Age: " << p.mAge << std::endl;
 }
 
 int main() {
-    Person j("John");
-    Person e = std::string("Emily");
-    Person f = 20;
+    Person j("John");   // Calls first constructor
+    Person e = std::string("Emily");    // Calls first constructor
+    Person f = 20;  // Calls second constructor
 
     printPerson(j);
     printPerson(e);
     printPerson(f);
+
+    // Compiler knows that printPerson function takes a constant Person reference
+    // So it has to create a Person object from given int argument. It implicitly
+    // calls the appropriate constructor to create the object
+    // In second example, same. But here we have a std::string argument
     printPerson(12);
-    //printPerson("Raskolnikov");
+    printPerson(std::string("Raskolnikov"));
 
     return 0;
 }
