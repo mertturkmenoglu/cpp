6c57f0439604285b3399f8ca98b07f98fd89c09a
Mert <mertturkmenoglu99@gmail.com>
2019-08-31 22:13:15 +0300

    Add constant expression example

---
+++ src/examples/advanced/E008_Constexpr/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+// time command can be used to measure running time
+// Ex: time ./program.out
+// Example time for const expr: 0,001
+// Example time for normal eval: 0,017
+
+/**
+ * Compiler will try to evaluate constant expressions
+ * at compile time so they will not be calculated at
+ * runtime
+ *
+ * Function calculates n-th fibonacci number
+ * @param n n-th number
+ * @return  fibonacci number
+ */
+constexpr long int fib(int n) {
+    return (n <= 1) ? n : fib(n-1) + fib(n-2);
+}
+
+/*
+ * Uncomment this to test
+ * long int fib(int n) {
+ *    return (n <= 1) ? n : fib(n-1) + fib(n-2);
+ * }
+ */
+
+int main() {
+    const long f = fib(30);
+    std::cout << f << std::endl;
+    return 0;
+}
